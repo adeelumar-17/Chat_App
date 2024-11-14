2b37cf5a39187ac39b07726996e431d3a0641e35
Adeel Umar <adeel@kali.kali>
2024-11-14 23:28:59 +0500

    add server functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,8 +19,18 @@ MainWindow::MainWindow(QWidget *parent)
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //implementation of server
+    TCPServer=new QTcpServer();
+    if(TCPServer->listen(QHostAddress::Any,8080)){
+        connect(TCPServer, &QTcpServer::newConnection, this, &MainWindow::newConnection);
+        qDebug()<<"Server started successfully";
+    }
+    else{
+        qDebug()<<"Server failed to start";
+    }
     //connection to local host
     //(QHostAddress(x.x.x.x),1234) for remote host
+    //implementation of client
     TCPSocket=new QTcpSocket();
     TCPSocket->connectToHost(QHostAddress::LocalHost,8080);
     connect(TCPSocket, &QTcpSocket::readyRead, this, &MainWindow::readDataFromServer);
@@ -40,6 +50,16 @@ void MainWindow::appendMessage(const QString &message){
     // Add the message to the chat history box with a new line
     ui->chatHistory->append(message);
 }
+
+void MainWindow::addNewClientConnection(QTcpSocket *socket)
+{
+    clientConnectionList.append(socket);
+    connect(socket, &QTcpSocket::readyRead, this, &MainWindow::readDataFromSocket);
+    ui->clientsList->addItem(QString::number(socket->socketDescriptor());
+    QString client="Client :"+QString::number(socket->socketDescriptor())+" connected with the server";
+    ui->chatHistory->append(client);
+}
+
 //write data to the server
 void MainWindow::on_sendMsgButton_clicked()
 {
@@ -57,6 +77,22 @@ void MainWindow::on_sendMsgButton_clicked()
     else{
         QMessageBox::information(this,"Error",TCPSocket->errorString());
     }
+    QString messageForClient=ui->msgData->text();
+    QString receiver=ui->clientsList->currentText();
+    if(ui->recipeintType->currentText()=="Everyone"){   //send to all connected clients
+        foreach(QTcpSocket *socket,clientConnectionList){
+                socket->write(messageForClient.toStdString().c_str());
+        }
+    }
+    else{
+        foreach(QTcpSocket *socket,clientConnectionList){     //send to selected clients only
+            if(socket->socketDescriptor()==receiver.toLongLong())
+            {
+                socket->write(messageForClient.toStdString().c_str());
+
+            }
+        }
+    }
 }
 //read data from the server
 void MainWindow::readDataFromServer()
@@ -65,6 +101,18 @@ void MainWindow::readDataFromServer()
         QByteArray Data_From_Server = TCPSocket->readAll();
         QString serverMsg = QString::fromUtf8(Data_From_Server);
         appendMessage(serverMsg);  // Display message in chat box
+    }
+}
+
+void MainWindow::readDataFromSocket()
+{
+
+}
+
+void MainWindow::newConnection()
+{
+    while(TCPServer->hasPendingConnections()){
+         addNewClientConnection(TCPServer->nextPendingConnection());
     }
 }
 
