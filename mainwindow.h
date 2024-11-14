a3ea4a4db60de2c483f871ed34c45139f9466289
Adeel Umar <adeel@kali.kali>
2024-11-14 23:28:59 +0500

    add server functionality

--- mainwindow.h
+++ mainwindow.h
@@ -3,7 +3,9 @@
 
 #include <QMainWindow>
 #include <QtNetwork/QTcpSocket>    //add necessary network libraries
+#include <QtNetwork/QTcpServer>
 #include <QtNetwork/QHostAddress>
+#include <QList>
 #include <QMessageBox>
 #include <QDebug>
 #include <QString>
@@ -25,13 +27,18 @@ public:
 private slots:
     void on_sendMsgButton_clicked();
     void readDataFromServer();
+    void readDataFromSocket();
+    void newConnection();
 
 private:
     Ui::MainWindow *ui;
     QString currentUser;
-    QTcpSocket *TCPSocket; //for tcp socket for reliable connections
+    QTcpSocket *TCPSocket; //tcp socket for reliable connections
+    QTcpServer *TCPServer; //for server functionality
+    QList<QTcpSocket*> clientConnectionList;
     void setupDatabase();
     void appendMessage(const QString &message);
+    void addNewClientConnection(QTcpSocket *socket);
 };
 
 #endif // MAINWINDOW_H
