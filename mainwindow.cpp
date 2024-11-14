af419233f8dddb6975dcb4e443121db9e289f706
Adeel Umar <adeel@kali.kali>
2024-11-14 23:59:47 +0500

    update server code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "database.h"
+
 void MainWindow::setupDatabase() {
     QString dbPath = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) + "/user_credentials.db";
     QDir().mkpath(QFileInfo(dbPath).path());  // Ensure directory exists
@@ -14,105 +15,115 @@ void MainWindow::setupDatabase() {
         qDebug() << "Database opened successfully at" << dbPath;
     }
 }
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //implementation of server
-    TCPServer=new QTcpServer();
-    if(TCPServer->listen(QHostAddress::Any,8080)){
+
+    // Implementation of server
+    TCPServer = new QTcpServer(this);
+    if (TCPServer->listen(QHostAddress::Any, 8080)) {
         connect(TCPServer, &QTcpServer::newConnection, this, &MainWindow::newConnection);
-        qDebug()<<"Server started successfully";
+        qDebug() << "Server started successfully";
+    } else {
+        qDebug() << "Server failed to start";
     }
-    else{
-        qDebug()<<"Server failed to start";
-    }
-    //connection to local host
-    //(QHostAddress(x.x.x.x),1234) for remote host
-    //implementation of client
-    TCPSocket=new QTcpSocket();
-    TCPSocket->connectToHost(QHostAddress::LocalHost,8080);
+
+    // Connection to local host
+    TCPSocket = new QTcpSocket(this);
+    TCPSocket->connectToHost(QHostAddress::LocalHost, 8080);
     connect(TCPSocket, &QTcpSocket::readyRead, this, &MainWindow::readDataFromServer);
-    TCPSocket->open(QIODevice::ReadWrite);
-    if(TCPSocket->isOpen()){
-        qDebug()<<"Connected to the server";
-    }
-    else{
-        qDebug()<<"Couldn't connect to the server";
+
+    if (TCPSocket->isOpen()) {
+        qDebug() << "Connected to the server";
+    } else {
+        qDebug() << "Couldn't connect to the server";
     }
 }
+
 MainWindow::~MainWindow()
 {
+    TCPServer->close();
+    for (QTcpSocket *client : clientConnectionList) {
+        client->disconnectFromHost();
+    }
     delete ui;
 }
-void MainWindow::appendMessage(const QString &message){
-    // Add the message to the chat history box with a new line
+
+void MainWindow::appendMessage(const QString &message) {
     ui->chatHistory->append(message);
 }
 
-void MainWindow::addNewClientConnection(QTcpSocket *socket)
-{
-    clientConnectionList.append(socket);
-    connect(socket, &QTcpSocket::readyRead, this, &MainWindow::readDataFromSocket);
-    ui->clientsList->addItem(QString::number(socket->socketDescriptor());
-    QString client="Client :"+QString::number(socket->socketDescriptor())+" connected with the server";
-    ui->chatHistory->append(client);
+QString MainWindow::formatMessage(const QString &sender, const QString &message) {
+    return sender + ": " + message;
 }
 
-//write data to the server
 void MainWindow::on_sendMsgButton_clicked()
 {
-    if(TCPSocket){
-        if(TCPSocket->isOpen()){
-            QString writeData=QString("@")+Login::userID+QString(":");
-            writeData+=ui->msgData->text();
-            TCPSocket->write(writeData.toUtf8());
-            appendMessage(writeData);
-        }
-        else{
-            QMessageBox::information(this,"Error",TCPSocket->errorString());
-        }
+    if (!TCPSocket || !TCPSocket->isOpen()) {
+        QMessageBox::information(this, "Error", TCPSocket ? TCPSocket->errorString() : "Socket not initialized.");
+        return;
     }
-    else{
-        QMessageBox::information(this,"Error",TCPSocket->errorString());
-    }
-    QString messageForClient=ui->msgData->text();
-    QString receiver=ui->clientsList->currentText();
-    if(ui->recipeintType->currentText()=="Everyone"){   //send to all connected clients
-        foreach(QTcpSocket *socket,clientConnectionList){
-                socket->write(messageForClient.toStdString().c_str());
-        }
-    }
-    else{
-        foreach(QTcpSocket *socket,clientConnectionList){     //send to selected clients only
-            if(socket->socketDescriptor()==receiver.toLongLong())
-            {
-                socket->write(messageForClient.toStdString().c_str());
 
+    QString writeData = "@" + Login::userID + ":" + ui->msgData->text();
+    TCPSocket->write(writeData.toUtf8());
+    appendMessage(writeData);
+
+    QString messageForClient = ui->msgData->text();
+    QString receiver = ui->clientsList->currentText();
+
+    if (ui->recipeintType->currentText() == "Everyone") {
+        for (QTcpSocket *socket : clientConnectionList) {
+            socket->write(messageForClient.toUtf8());
+        }
+    } else {
+        for (QTcpSocket *socket : clientConnectionList) {
+            if (socket->socketDescriptor() == receiver.toLongLong()) {
+                socket->write(messageForClient.toUtf8());
+                break;
             }
         }
     }
 }
-//read data from the server
+
 void MainWindow::readDataFromServer()
 {
     if (TCPSocket && TCPSocket->isOpen()) {
         QByteArray Data_From_Server = TCPSocket->readAll();
         QString serverMsg = QString::fromUtf8(Data_From_Server);
-        appendMessage(serverMsg);  // Display message in chat box
+        appendMessage(serverMsg);
     }
 }
 
 void MainWindow::readDataFromSocket()
 {
-
+    QTcpSocket *socket = reinterpret_cast<QTcpSocket*>(sender());
+    QByteArray messageFromServer = socket->readAll();
+    QString message = "Client " + QString::number(socket->socketDescriptor()) + " :: " + QString::fromUtf8(messageFromServer);
+    appendMessage(message);
 }
 
 void MainWindow::newConnection()
 {
-    while(TCPServer->hasPendingConnections()){
-         addNewClientConnection(TCPServer->nextPendingConnection());
+    while (TCPServer->hasPendingConnections()) {
+        QTcpSocket *newClient = TCPServer->nextPendingConnection();
+        connect(newClient, &QTcpSocket::readyRead, this, &MainWindow::readDataFromSocket);
+        connect(newClient, &QTcpSocket::disconnected, this, [=] { removeClient(newClient); });
+        clientConnectionList.append(newClient);
+        ui->clientsList->addItem(QString::number(newClient->socketDescriptor()));
+        appendMessage("Client " + QString::number(newClient->socketDescriptor()) + " connected");
     }
 }
 
+void MainWindow::removeClient(QTcpSocket *socket)
+{
+    clientConnectionList.removeAll(socket);
+    ui->clientsList->clear();  // Refresh clients list
+    for (QTcpSocket *client : clientConnectionList) {
+        ui->clientsList->addItem(QString::number(client->socketDescriptor()));
+    }
+    socket->deleteLater();
+    appendMessage(QString("Client %1 disconnected").arg(socket->socketDescriptor()));
+}
